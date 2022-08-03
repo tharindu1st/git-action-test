49700a11225ac5902a162142092bd2d7461807e7
tharindud <tharindua@wso2.com>
2022-08-03 23:43:48 +0530

    add checkstyle.yaml

--- src/main/java/CalculatorUtil.java
+++ src/main/java/CalculatorUtil.java
@@ -1,3 +1,6 @@
+/**
+ *
+ */
 public class CalculatorUtil {
 
     public static long add(long x, long y) {
@@ -13,6 +16,9 @@ public class CalculatorUtil {
     }
 
     public static long divide(long x, long y) {
+        return x / y;
+    }
+    public static long abc(long x,long y) {
         return x / y;
     }
 }
--- src/test/java/CalculatorUtilTest.java
+++ src/test/java/CalculatorUtilTest.java
@@ -1,20 +1,19 @@
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import static org.testng.Assert.*;
-
 public class CalculatorUtilTest {
 
     @Test
     public void testAdd() {
-        long x = 1l;
-        long y = 2l;
-        Assert.assertEquals(CalculatorUtil.add(x,y),3);
+        long x = 1L;
+        long y = 2L;
+        Assert.assertEquals(CalculatorUtil.add(x, y), 3);
     }
+
     @Test
     public void testAddNegative() {
-        long x = 1l;
-        long y = 2l;
-        Assert.assertFalse(CalculatorUtil.add(x,y)!=5);
+        long x = 1L;
+        long y = 2L;
+        Assert.assertFalse(CalculatorUtil.add(x, y) != 5);
     }
 }
